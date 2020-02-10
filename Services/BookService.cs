db9d37a9cba0afaba1792c1faaf60a37978b308f
John Hui <mjhui90@gmail.com>
2020-02-09 22:48:44 -0800

    added current iles

---
+++ Services/BookService.cs
@@ -0,0 +1,12 @@
+﻿using BooksApi.Models;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BooksApi.Services
+{
+    public class BookService
+    {
+        private readonly IMongoCollection<Book> _books;
+    }
+}
